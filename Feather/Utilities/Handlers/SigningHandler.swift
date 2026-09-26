7078b053c0af262809a48e4d3898d42b0d35fedf
C <claration@riseup.net>
2026-09-25 17:46:05 -0700

    feat(Feather): entitlement merging (#723)
    fix: signing-options not saving when a new value is added

--- Feather/Utilities/Handlers/SigningHandler.swift
+++ Feather/Utilities/Handlers/SigningHandler.swift
@@ -103,6 +103,10 @@ final class SigningHandler: NSObject {
 		// iOS "26" (19) needs special treatment
 		try await _locateMachosAndFixupArm64eSlice(for: movedAppPath)
 		
+		if _options.mergeEntitlements && _options.appEntitlementsFile == nil {
+			try await _mergeEntitlements(for: movedAppPath, with: _options)
+		}
+		
 		let handler = ZsignHandler(appUrl: movedAppPath, options: _options, cert: appCertificate)
 		try await handler.disinject()
 		
@@ -405,6 +409,9 @@ extension SigningHandler {
 		_enumerateFiles(at: app) { $0.hasSuffix("_CodeSignature") }
 	}
 	
+	// this is mainly for CydiaSubstrate where it often contains legacy arm64e slices
+	// which cause the app to crash when loaded on ios 26+,
+	// avoids `(cpu type/subtype in slice (arm64e.old) does not match fat header (arm64e))`
 	private func _locateMachosAndFixupArm64eSlice(for app: URL) async throws {
 		let machoFiles = _enumerateFiles(at: app) {
 			$0.hasSuffix(".dylib") || $0.hasSuffix(".framework")
@@ -425,6 +432,115 @@ extension SigningHandler {
 				continue
 			}
 		}
+	}
+	
+	// shit ass code
+	private func _mergeEntitlements(for app: URL, with options: Options) async throws {
+		guard let appBinPath = Bundle(url: app)?.executableURL else {
+			throw SigningFileHandlerError.appNotFound
+		}
+		
+		guard 
+			let binaryProfile = LCGetMachOEntitlements(appBinPath.path),
+			let binaryEntitlementsDict = (try? PropertyListSerialization.propertyList(
+				from: binaryProfile,
+				options: [],
+				format: nil
+			)) as? [String: Any]
+		else {
+			return
+		}
+		
+		guard 
+			let cert = self.appCertificate, 
+			let ourEntitlements = CertificateReader(Storage.shared.getFile(.provision, from: cert)).decoded
+		else {
+			return 
+		}
+		
+		let ourTeamIdentifier = ourEntitlements.TeamIdentifier[0]
+		let ourBundleIdentifer = options.appIdentifier ?? _app.identifier
+		
+		var baseDictionary: [String: Any] = (ourEntitlements.Entitlements ?? [:]).mapValues { $0.value }
+		let additionsDictionary: [String: Any] = binaryEntitlementsDict
+
+		
+		// replaces wildcards in base entitlements with new application id
+		// aggressive approach though, lets just hope this works :)
+		if let ourBundleIdentifer {
+			func replaceWildcards(in value: Any, with newAppID: String) -> Any {
+				if let stringValue = value as? String {
+					return stringValue.replacingOccurrences(of: "*", with: newAppID)
+				} else if let arrayValue = value as? [Any] {
+					return arrayValue.map { replaceWildcards(in: $0, with: newAppID) }
+				} else if let dictValue = value as? [String: Any] {
+					return dictValue.mapValues { replaceWildcards(in: $0, with: newAppID) }
+				} else if let anyCodable = value as? AnyCodable {
+					let substituted = replaceWildcards(in: anyCodable.value, with: newAppID)
+					return AnyCodable(substituted)
+				}
+				return value
+			}
+			
+			baseDictionary = baseDictionary.reduce(into: [:]) { result, entry in
+				let (key, value) = entry
+
+				if 
+					key == "com.apple.developer.associated-domains" ||
+					key == "application.identifier"
+				{
+					result[key] = value
+					return
+				}
+
+				result[key] = replaceWildcards(
+					in: value,
+					with: ourBundleIdentifer
+				)
+			}
+		}
+		
+		// this will not consider app extensions, zsign doesnt have functionality be more specific
+		// with provisioning profiles, this works fine for impactors "Only register main bundle" though.
+		if let keychainGroups = additionsDictionary["keychain-access-groups"] as? [Any] {
+			baseDictionary["keychain-access-groups"] = keychainGroups
+		}
+		
+		let regex = try NSRegularExpression(pattern: "^[A-Z0-9]{10}\\.")
+		
+		if let groups = baseDictionary["keychain-access-groups"] as? [String] {
+			// remove anything that does not match XXXXXXXXXX. (for example, com.apple.token)
+			// only XXXXXXXXXX.* is allowed on keychain-access-groups
+			let validGroups = groups.filter { group in
+				let range = NSRange(location: 0, length: group.utf16.count)
+				return regex.firstMatch(in: group, options: [], range: range) != nil
+			}
+			
+			let updatedGroups = validGroups.map { group -> String in
+				let range = NSRange(location: 0, length: group.utf16.count)
+				if regex.firstMatch(in: group, options: [], range: range) != nil && group.count >= 11 {
+					let suffix = group.suffix(from: group.index(group.startIndex, offsetBy: 11))
+					return "\(ourTeamIdentifier).\(suffix)"
+				}
+				return group
+			}
+			
+			baseDictionary["keychain-access-groups"] = updatedGroups
+		}
+		
+		let plistData = try PropertyListSerialization.data(
+			fromPropertyList: baseDictionary,
+			format: .xml,
+			options: 0
+		)
+
+		let tempEntitlementsURL = _uniqueWorkDir
+			.appendingPathComponent(UUID().uuidString)
+			.appendingPathExtension("plist")
+		
+		try plistData.write(to: tempEntitlementsURL, options: .atomic)
+		
+		_options.appEntitlementsFile = tempEntitlementsURL
 	}
 	
 	private func _enumerateFiles(at base: URL, where predicate: (String) -> Bool) -> [URL] {
